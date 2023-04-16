5613a27ed720f3ca3789b7ae8af0be1f529c5e32
sajjany47 <sajjany47@gmail.com>
2023-04-16 22:36:38 +0530

    api push

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "s-a-j-j-a-n-!@#$%%^&*()_123",
+};
---
+++ app/routes/auth.routes.js
@@ -0,0 +1,19 @@
+const { verifySignUp } = require("../middlewares");
+const controller = require("../controllers/auth.controller");
+
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+    next();
+  });
+
+  app.post(
+    "/api/auth/signup"[
+      (verifySignUp.checkDuplicateUsernameOrEmail,
+      verifySignUp.checkRolesExisted)
+    ],
+    controller.singup
+  );
+  app.post("/api/auth/sigin", controller.sigin);
+  app.post("api/auth/signout", controller.signout);
+};
